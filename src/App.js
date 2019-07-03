24550eb71114b7a4a98ad6258b78b26000338f68
Rokas Tamosevicius <rtamose@unifysquare.com>
2019-07-03 17:56:26 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hi Rokas!!! 3333
+          KUKUL... I miss you node
+          I was changed
         </p>
         <a
           className="App-link"
